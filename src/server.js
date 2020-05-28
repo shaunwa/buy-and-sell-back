2f9df75ed366d6e6a4e079558d7edfe548ec5897
Shaun W <shaun.p.wassell@gmail.com>
2020-05-28 15:43:20 -0400

    02.02 End

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: (req, h) => {
+            return 'Hello!';
+        }
+    });
+
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
+});
+
+start();
