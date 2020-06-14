437e965dbed050ceea274fb2a236c314543fe1a4
Shaun W <shaun.p.wassell@gmail.com>
2020-06-14 13:51:14 -0400

    05.06 End

--- src/routes/getUserListings.js
+++ src/routes/getUserListings.js
@@ -1,10 +1,16 @@
+import * as admin from 'firebase-admin';
 import { db } from '../database';
+import Boom from '@hapi/boom';
 
 export const getUserListingsRoute = {
     method: 'GET',
     path: '/api/users/{userId}/listings',
     handler: async (req, h) => {
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
         const userId = req.params.userId;
+
+        if (user.user_id !== userId) throw Boom.unauthorized('Users can only access their own listings!');
 
         const { results } = await db.query(
             'SELECT * FROM listings WHERE user_id=?',
