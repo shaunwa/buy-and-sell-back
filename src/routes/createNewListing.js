b1ba4931a3a7e6892039f031ce95b76b00a4c534
Shaun W <shaun.p.wassell@gmail.com>
2020-06-14 14:23:52 -0400

    05.07 End

--- src/routes/createNewListing.js
+++ src/routes/createNewListing.js
@@ -1,13 +1,16 @@
 import { v4 as uuid } from 'uuid';
+import * as admin from 'firebase-admin';
 import { db } from '../database';
 
 export const createNewListingRoute = {
     method: 'POST',
     path: '/api/listings',
     handler: async (req, h) => {
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
+        const userId = user.user_id;
         const id = uuid();
         const { name = '', description = '', price = 0 } = req.payload;
-        const userId = '12345';
         const views = 0;
 
         await db.query(`
